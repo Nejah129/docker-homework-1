c2d9430352166b71d0e1d025cd1b1e0f6e21a941
Nejah129 <nejahyenguiwork@gmail.com>
2024-01-27 11:58:19 +0100

    adding docker compose

--- server.js
+++ server.js
@@ -16,20 +16,20 @@ var app=express()
 //     res.send(students.filter(el=>el.id==req.params.id))
 // });
 
-app.use((req,res,next)=>{
-    console.log("Time",Date.now())
-    var date=new Date;
-    var day=date.getDay();
-    var hour=date.getHours();
-    console.log("date ",date,"Hours ",hour,"Day ",day)
-    if(hour>9 && hour<17 && day>0 && day < 6) {next()} else {
+// app.use((req,res,next)=>{
+//     console.log("Time",Date.now())
+//     var date=new Date;
+//     var day=date.getDay();
+//     var hour=date.getHours();
+//     console.log("date ",date,"Hours ",hour,"Day ",day)
+//     if(hour>9 && hour<17 && day>0 && day < 6) {next()} else {
 
-        res.send("<h1>oups it is closed come back later</h1>")
-    }
+//         res.send("<h1>oups it is closed come back later</h1>")
+//     }
 
-})
+// })
 // express middleware
- app.use(express.static("pages"))
+ app.use(express.static("pages"));
 
 
 // const requestTime = function (req, res, next) {
